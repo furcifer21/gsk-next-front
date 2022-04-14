3304ae338c86af5746e67091575564b63d3622e5
agorev <agorev@tango.com.ua>
2022-04-14 17:58:27 +0300

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import '/css/style.css';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 // We need this check because bootstrap looking for a window that is absent on the server
@@ -6,7 +7,6 @@ if (typeof window !== 'undefined') {
     require("@popperjs/core");
     require("bootstrap");
 }
-import '../resources/css/style.css';
 
 const App = ({ Component, pageProps }) => {
     return(
